78ef72fffb518a28a18f20fb2da29f2578280ba9
unknown <anuj.khatakho2@gmail.com>
2025-01-22 08:10:21 +0545

    updated assisgnment 2

---
+++ Assignment 2/config/config.mjs
@@ -0,0 +1,8 @@
+export const config = {
+    port:'777',
+    URL : 'this.url'
+}
+
+export function helper(config){
+    console.log(config.URL.toUpperCase());
+}
---
+++ Assignment 2/config/main.js
@@ -0,0 +1,5 @@
+import { config, helper } from "./config.mjs";
+
+helper(config);
+
+console.log(config);
